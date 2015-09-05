a11bf44f9a019cbdb4fd4e7eb9c2f6e5e98b567c
Ryan Delaney <rrdelaney@outlook.com>
2015-09-05 14:50:56 -0400

    Added new attendence, updated info

---
+++ attendence/index.php
@@ -0,0 +1,55 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        date_default_timezone_set('EST');
+        $date = getdate();
+        $month = $date['mon'];
+        $day = $date['mday'];
+        $year = $date['year'];
+        $rcs = $_POST['rcs'];
+
+        file_put_contents("attendence-$month-$day-$year.txt", "$rcs\n", FILE_APPEND);
+        $added = true;
+    } else {
+        $added = false;
+    }
+?>
+
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+        <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/paper/bootstrap.min.css" rel="stylesheet" integrity="sha256-hMIwZV8FylgKjXnmRI2YY0HLnozYr7Cuo1JvRtzmPWs= sha512-k+wW4K+gHODPy/0gaAMUNmCItIunOZ+PeLW7iZwkDZH/wMaTrSJTt7zK6TGy6p+rnDBghAxdvu1LX2Ohg0ypDw==" crossorigin="anonymous">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha256-k2/8zcNbxVIh5mnQ52A0r3a6jAgMGxFJFE2707UxGCk= sha512-ZV9KawG2Legkwp3nAlxLIVFudTauWuBpC10uEafMHYL0Sarrz5A7G79kXh5+5+woxQ5HM559XX2UZjMJ36Wplg==" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+
+        <style>
+            body {
+                padding-top: 50px;
+                max-width: 600px;
+                margin: 0 auto;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php if ($added): ?>
+                <div class="alert alert-success alert-dismissable" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <strong>Success!</strong> Thanks for signing in!
+                </div>
+            <?php endif; ?>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <form method="post">
+                        <div class="form-group">
+                            <label for="rcs">RCS ID</label>
+                            <input type="text" class="form-control" name="rcs">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Sign In!</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
